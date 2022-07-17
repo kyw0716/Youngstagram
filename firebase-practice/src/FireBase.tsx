fe6acf0fc994a4736456a5af06492425febe44fb
kyw0716 <kyw0716@naver.com>
2022-07-17 15:13:54 +0900

    feat: FireBase init

---
+++ firebase-practice/src/FireBase.tsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC7IT7tMtvVzSI2tj7JSPfdU3tZSgXCcG4",
+  authDomain: "fir-practice-d0e2e.firebaseapp.com",
+  projectId: "fir-practice-d0e2e",
+  storageBucket: "fir-practice-d0e2e.appspot.com",
+  messagingSenderId: "116460764746",
+  appId: "1:116460764746:web:73f450e7b7442ff3a230b3",
+};
+
+export default initializeApp(firebaseConfig);
