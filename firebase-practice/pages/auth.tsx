c16f6cde3d362580ac9c253548e4ede1758d2caa
kyw0716 <kyw0716@naver.com>
2022-07-17 22:30:34 +0900

    feat: 로그인, 회원가입 기능 추가

---
+++ firebase-practice/pages/auth.tsx
@@ -0,0 +1,88 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth"
+import { useRouter } from "next/router"
+import { useState } from "react"
+import { authService } from "../src/FireBase"
+
+export default function Auth() {
+  const router = useRouter()
+  const [Email, setEmail] = useState<string>("")
+  const [Password, setPassword] = useState<string>("")
+  const [isNewAccount, setIsNewAccount] = useState<boolean>(false)
+  const handleOnInputChange: React.ChangeEventHandler<HTMLInputElement> = (
+    event,
+  ) => {
+    if (event.target.name === "Email") {
+      setEmail(event.target.value)
+      return
+    }
+    setPassword(event.target.value)
+  }
+  const handleOnSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault()
+    if (isNewAccount === true) {
+      createUserWithEmailAndPassword(authService, Email, Password)
+        .then((response) => {
+          if ((response.operationType = "signIn")) {
+            signOut(authService)
+            setIsNewAccount(false)
+            alert(
+              "회원가입에 성공 하셨습니다! 회원가입하신 정보로 로그인 바랍니다.",
+            )
+            setEmail("")
+            setPassword("")
+          }
+        })
+        .catch((error) => alert(error))
+      return
+    }
+    signInWithEmailAndPassword(authService, Email, Password).then(
+      (response) => {
+        if (response.operationType === "signIn") {
+          router.push("/")
+        }
+      },
+    )
+  }
+
+  return (
+    <>
+      <form onSubmit={handleOnSubmit}>
+        <input
+          type={"text"}
+          placeholder="Email"
+          required
+          onChange={handleOnInputChange}
+          name="Email"
+          value={Email}
+        />
+        <input
+          type={"password"}
+          placeholder="Password"
+          required
+          onChange={handleOnInputChange}
+          name="Password"
+          value={Password}
+        />
+        <input
+          type={"submit"}
+          value={isNewAccount ? "Create Account" : "Log in"}
+        />
+      </form>
+      <button
+        onClick={() => {
+          setIsNewAccount(true)
+        }}
+      >
+        회원가입
+      </button>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with GitHub</button>
+      </div>
+    </>
+  )
+}
--- firebase-practice/pages/index.tsx
+++ firebase-practice/pages/index.tsx
@@ -1,8 +1,35 @@
-import type { NextPage } from "next";
-import FireBase from "../src/FireBase";
+import { onAuthStateChanged, signOut } from "firebase/auth"
+import type { NextPage } from "next"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+import { authService } from "../src/FireBase"
 
 const Home: NextPage = () => {
-  return <>FireBase 연습</>;
-};
+  const router = useRouter()
+  const [currentUserEmail, setCurrentUserEmail] = useState<string>("")
+  useEffect(() => {
+    onAuthStateChanged(authService, (user) => {
+      if (user) {
+        if (user.email !== null) setCurrentUserEmail(user.email)
+        return
+      }
+      router.push("/auth")
+    })
+    /*eslint-disable-next-line*/
+  }, [])
 
-export default Home;
+  return (
+    <>
+      <span>환영합니다!! {currentUserEmail}님!</span>
+      <button
+        onClick={() => {
+          signOut(authService)
+        }}
+      >
+        로그아웃
+      </button>
+    </>
+  )
+}
+
+export default Home
