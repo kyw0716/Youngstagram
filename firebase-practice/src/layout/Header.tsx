62a611d0dc3b0f34190875ea5087d1af8da14d35
kyw0716 <kyw0716@naver.com>
2022-07-24 15:44:42 +0900

    feat: 헤더에 메뉴, 로고 추가

--- firebase-practice/src/layout/Header.tsx
+++ firebase-practice/src/layout/Header.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from "next/router"
+import { useState } from "react"
 import styled from "styled-components"
 import { Margin } from "ui"
 
@@ -19,16 +21,48 @@ const Style = {
     display: flex;
     justify-content: space-between;
     align-items: center;
+    position: relative;
+  `,
+  LineMenu: styled.img`
+    width: 50px;
+    height: 30px;
+    cursor: pointer;
+  `,
+  Logo: styled.h3`
+    cursor: pointer;
+    padding: 16px 0px;
+    margin: 0;
+  `,
+  DropDownMenu: styled.div`
+    width: 107px;
+    height: 60px;
+    border: 1px solid lightgrey;
+    position: absolute;
+    right: -28px;
+    top: 60px;
+    background-color: white;
   `,
 }
 
 export default function Header() {
+  const router = useRouter()
+  const [isMeunOpen, setIsMenuOpen] = useState<boolean>(false)
+  const handleMenuOpen = () => {
+    setIsMenuOpen((current) => !current)
+  }
   return (
     <>
       <Style.Container>
         <Style.Nav>
-          <h3>youngstagram</h3>
-          <span>메뉴들</span>
+          <Style.Logo
+            onClick={() => {
+              router.push("/")
+            }}
+          >
+            youngstagram
+          </Style.Logo>
+          <Style.LineMenu src="/line-menu.svg" onClick={handleMenuOpen} />
+          {isMeunOpen ? <Style.DropDownMenu /> : <></>}
         </Style.Nav>
       </Style.Container>
       <Margin direction="column" size={60} />
