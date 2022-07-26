906b0931005c1ecbf9130dfca354ccc758b85f6d
kyw0716 <kyw0716@naver.com>
2022-07-26 23:01:02 +0900

    feat: test 페이지 코드 추가

--- firebase-practice/pages/index.tsx
+++ firebase-practice/pages/index.tsx
@@ -1,28 +1,12 @@
-import { User } from "firebase/auth"
 import type { NextPage } from "next"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import { authService, DBService } from "@FireBase"
 import { doc, DocumentData, onSnapshot } from "firebase/firestore"
 import styled from "styled-components"
-import ImageCard from "@share/ImageCard"
 import Header from "components/layout/Header"
 import { Margin } from "ui"
-
-const Style = {
-  MainPageImage: styled.img`
-    width: 200px;
-    height: 200px;
-  `,
-  ImageContainer: styled.div`
-    display: flex;
-    width: 100vw;
-    align-items: center;
-    flex-direction: column;
-    gap: 20px;
-    padding-bottom: 50px;
-  `,
-}
+import ImageList from "@share/ImageList"
 
 const Home: NextPage = () => {
   const router = useRouter()
@@ -51,28 +35,14 @@ const Home: NextPage = () => {
     <>
       <Header />
       <Margin direction="column" size={30} />
-      <Style.ImageContainer>
-        {imageData &&
-          imageData.map((imageObject, index) => {
-            return (
-              <>
-                {!imageObject.private && (
-                  <div key={index}>
-                    <ImageCard
-                      key={index}
-                      imageUrl={imageObject.image}
-                      imageTitle={imageObject.imageTitle}
-                      isPrivate={imageObject.private}
-                      userId={""}
-                      userName={imageObject.creator}
-                      isMainPage={true}
-                    />
-                  </div>
-                )}
-              </>
-            )
-          })}
-      </Style.ImageContainer>
+      {authService.currentUser?.uid !== undefined &&
+        authService.currentUser.displayName !== null && (
+          <ImageList
+            imageData={imageData.filter((data) => !data.private)}
+            isMainPage={true}
+            userId={authService.currentUser?.uid}
+          />
+        )}
     </>
   )
 }
