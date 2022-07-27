9fb0bd9dbf6037f7f75e5f79859583f9fae9e0d3
kyw0716 <kyw0716@naver.com>
2022-07-28 07:22:10 +0900

    feat: nav 헤더 메뉴 css 추가

--- firebase-practice/src/components/layout/Header.tsx
+++ firebase-practice/src/components/layout/Header.tsx
@@ -13,7 +13,7 @@ const Style = {
     background-color: white;
     position: fixed;
     top: 0;
-    z-index: 9999999;
+    z-index: 5;
     display: flex;
     justify-content: center;
   `,
@@ -47,33 +47,60 @@ const Style = {
   `,
   DropDownMenu: styled.div`
     width: 150px;
-    height: 60px;
-    border: 1px solid lightgrey;
+    height: 100px;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 10px 0px;
     position: absolute;
     right: -28px;
-    top: 60px;
+    top: 65px;
     background-color: white;
     display: flex;
     flex-direction: column;
     align-items: center;
+    border-radius: 9px;
+    z-index: 7;
   `,
-  MenuItem: styled.div`
-    width: 148px;
-    height: 58px;
-    -webkit-appearance: none;
+  ProfileButton: styled.div`
+    width: 150px;
+    height: 50px;
     border: none;
     color: #5f5f5f;
     font-size: 15px;
     display: flex;
-    justify-content: center;
     align-items: center;
     gap: 8px;
+    padding-left: 10px;
+    border-radius: 9px 9px 0px 0px;
     &:hover {
-      background-color: #dcdcdc;
-      width: 150px;
-      border: 1px solid #dcdcdc;
+      background-color: #f0f0f0;
     }
     cursor: pointer;
+  `,
+  LogoutButton: styled.div`
+    width: 150px;
+    height: 50px;
+    border: none;
+    color: #5f5f5f;
+    font-size: 15px;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    padding-left: 10px;
+    border-radius: 0px 0px 9px 9px;
+    &:hover {
+      background-color: #f0f0f0;
+    }
+    cursor: pointer;
+  `,
+  ChatBalloon: styled.div`
+    width: 25px;
+    height: 25px;
+    transform: rotate(45deg);
+    background-color: white;
+    position: absolute;
+    right: 13px;
+    top: 55px;
+    z-index: 6;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 7px 3px;
   `,
   Icon: styled.img`
     width: 15px;
@@ -99,34 +126,37 @@ export default function Header() {
           </Style.Logo>
           <Style.LineMenu src="/line-menu.svg" onClick={handleMenuOpen} />
           {isMeunOpen ? (
-            <Style.DropDownMenu
-              onMouseLeave={() => {
-                setIsMenuOpen(false)
-              }}
-            >
-              <Style.MenuItem
-                onClick={() => {
-                  signOut(authService)
-                  router.push("/auth")
+            <>
+              <Style.DropDownMenu
+                onMouseLeave={() => {
+                  setIsMenuOpen(false)
                 }}
               >
-                <Style.Icon src="/logout.svg" alt="logout" />
-                로그아웃
-              </Style.MenuItem>
-              <Style.MenuItem
-                onClick={() => {
-                  if (
-                    authService.currentUser?.uid !== null &&
-                    authService.currentUser?.uid !== undefined
-                  ) {
-                    router.push(`/u/${authService.currentUser?.uid}`)
-                  }
-                }}
-              >
-                <Style.Icon src="/profile.svg" alt="profile" />
-                프로필 페이지
-              </Style.MenuItem>
-            </Style.DropDownMenu>
+                <Style.ProfileButton
+                  onClick={() => {
+                    if (
+                      authService.currentUser?.uid !== null &&
+                      authService.currentUser?.uid !== undefined
+                    ) {
+                      router.push(`/u/${authService.currentUser?.uid}`)
+                    }
+                  }}
+                >
+                  <Style.Icon src="/profile.svg" alt="profile" />
+                  프로필
+                </Style.ProfileButton>
+                <Style.LogoutButton
+                  onClick={() => {
+                    signOut(authService)
+                    router.push("/auth")
+                  }}
+                >
+                  <Style.Icon src="/logout.svg" alt="logout" />
+                  로그아웃
+                </Style.LogoutButton>
+              </Style.DropDownMenu>
+              <Style.ChatBalloon />
+            </>
           ) : (
             <></>
           )}
