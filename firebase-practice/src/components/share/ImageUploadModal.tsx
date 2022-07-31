bf89e39c55bf50f8f52e2953f9ee99f739196ca1
kyw0716 <kyw0716@naver.com>
2022-07-31 18:36:45 +0900

    feat: 위치 입력받을 수 있도록 스타일 변경

--- firebase-practice/src/components/share/ImageUploadModal.tsx
+++ firebase-practice/src/components/share/ImageUploadModal.tsx
@@ -37,7 +37,7 @@ const Style = {
     height: 494px;
     display: flex;
     flex-direction: column;
-    padding-top: 20px;
+    padding-top: 10px;
     align-items: center;
     position: relative;
   `,
@@ -73,6 +73,28 @@ const Style = {
     bottom: 15px;
     right: 15px;
   `,
+  LocationInputSection: styled.div`
+    width: 100%;
+    padding-right: 10px;
+    border-top: 1px solid lightgrey;
+    border-bottom: 1px solid lightgrey;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `,
+  LocationInput: styled.input`
+    width: 291px;
+    height: 44px;
+    border: none;
+    padding-left: 10px;
+    ::placeholder {
+      color: lightgrey;
+      font-size: 15px;
+    }
+    :focus {
+      outline: none;
+    }
+  `,
 }
 
 export default function ImageUploadModal({ setIsOpen, isOpen }: Props) {
@@ -89,6 +111,8 @@ export default function ImageUploadModal({ setIsOpen, isOpen }: Props) {
   const [isFileExist, setIsFileExist] = useState<boolean>(false)
   const [imagePreviewSrc, setImagePreviewSrc] = useState<string>("")
   const [windowSize, setWindowSize] = useState<number>(0)
+
+  const [desc, setDesc] = useState<string>("")
   useEffect(() => {
     setWindowSize(window.innerWidth)
     window.addEventListener("resize", () => {
@@ -137,7 +161,7 @@ export default function ImageUploadModal({ setIsOpen, isOpen }: Props) {
         >
           <Image
             width={windowSize < 784 ? "95%" : 494}
-            height={windowSize < 784 ? 250 : 494}
+            height={windowSize < 784 ? 200 : 494}
             style={{
               boxShadow: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
               maxWidth: 495,
@@ -179,9 +203,35 @@ export default function ImageUploadModal({ setIsOpen, isOpen }: Props) {
             <Margin direction="column" size={10} />
             <Style.TextArea
               placeholder="문구 입력..."
-              style={windowSize < 784 ? { height: 150, width: "100%" } : {}}
+              style={windowSize < 784 ? { height: 100, width: "100%" } : {}}
+              maxLength={2200}
+              value={desc}
+              onChange={(event) => {
+                setDesc(event.target.value)
+              }}
             />
+            <Margin direction="column" size={8} />
+            <FlexBox
+              width={windowSize < 784 ? "100%" : 310}
+              justifyContents={"flex-end"}
+              alignItems="center"
+              style={{ color: "lightgrey" }}
+            >
+              {desc.length}/2200
+            </FlexBox>
+            <Margin direction="column" size={windowSize < 784 ? 0 : 8} />
+            <Style.LocationInputSection>
+              <Style.LocationInput placeholder="위치 추가" />
+              <Margin direction="row" size={10} />
+              <Image
+                src={"/location.svg"}
+                width={16}
+                height={16}
+                alt="location"
+              />
+            </Style.LocationInputSection>
           </Style.InputSection>
+
           <Style.SubmitButton
             onClick={() => {
               alert("아직 기능 구현은 안했어용")
