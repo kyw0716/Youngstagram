f9594cea02ab8f86434fc49b8821ee8557123e72
kyw0716 <kyw0716@naver.com>
2022-07-31 16:15:57 +0900

    feat: dto 대공사 시작

--- firebase-practice/src/backend/dto.tsx
+++ firebase-practice/src/backend/dto.tsx
@@ -5,3 +5,21 @@ export type UserImageDataAll = {
   creator: string
   creatorProfile: string
 }
+//TODO: 이름 비교를 통해 삭제하던 방식을 id를 비교하여 삭제하는 방식으로 바꾸기
+export type Creator = {
+  name: string
+  id: string
+  ProfileImage: string
+}
+
+//TODO: dto 바뀐거 적용시켜서 재배포하기
+// 이미지 주소, 이미지에 있는 설명, 해당 이미지의 장소(얜 써도되고 아니어도 됨), 공개 비공개 여부, 저장소 id
+// 이미지를 생성한 사람의 정보(이름, id, 프로필 이미지 주소)
+export type UserImageDataAllDemo = {
+  image: string
+  imageDesc: string
+  location: string
+  private: boolean
+  storageId: string
+  creator: Creator
+}
