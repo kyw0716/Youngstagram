5416eb0bcd8d08a4b97a5aa371058faff9f46be4
kyw0716 <kyw0716@naver.com>
2022-08-03 17:42:59 +0900

    feat: TODO 지우기

--- firebase-practice/src/backend/dto.tsx
+++ firebase-practice/src/backend/dto.tsx
@@ -1,20 +1,8 @@
-// export type UserImageDataAll = {
-//   image: string
-//   imageTitle: string
-//   private: boolean
-//   creator: string
-//   creatorProfile: string
-// }
-//TODO: 이름 비교를 통해 삭제하던 방식을 id를 비교하여 삭제하는 방식으로 바꾸기
 export type Creator = {
   name: string | null
   id: string
   profileImage: string | null
 }
-
-//TODO: dto 바뀐거 적용시켜서 재배포하기
-// 이미지 주소, 이미지에 있는 설명, 해당 이미지의 장소(얜 써도되고 아니어도 됨), 공개 비공개 여부, 저장소 id
-// 이미지를 생성한 사람의 정보(이름, id, 프로필 이미지 주소)
 export type UserImageDataAll = {
   imageUrl: string
   desc: string
@@ -22,12 +10,4 @@ export type UserImageDataAll = {
   private: boolean
   storageId: string
   creator: Creator
-}
-
-export type UserImageData = {
-  imageUrl: string
-  desc: string
-  location: string
-  private: boolean
-  storageId: string
 }
