4921cb5981026ac92efd84669a504762412d6088
kyw0716 <kyw0716@naver.com>
2022-08-08 23:36:56 +0900

    feat: 로그인 여부 확인 페이지 변경

--- firebase-practice/pages/_app.tsx
+++ firebase-practice/pages/_app.tsx
@@ -1,7 +1,21 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
+import { useEffect } from "react"
+import { useRouter } from "next/router"
+import { onAuthStateChanged } from "firebase/auth"
+import { authService } from "@FireBase"
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    onAuthStateChanged(authService, (user) => {
+      if (user) {
+        router.push("/")
+      } else {
+        router.push("/auth")
+      }
+    })
+  }, [])
   return <Component {...pageProps} />
 }
 
