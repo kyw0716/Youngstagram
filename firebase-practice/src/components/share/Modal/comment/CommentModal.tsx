774218053c58a1cc9987ab753700112f55ef0e44
kyw0716 <kyw0716@naver.com>
2022-09-03 23:33:55 +0900

    feat: windowSize => @media로 스타일 구성 변경

--- firebase-practice/src/components/share/Modal/comment/CommentModal.tsx
+++ firebase-practice/src/components/share/Modal/comment/CommentModal.tsx
@@ -24,74 +24,42 @@ type Props = {
 }
 
 const Style = {
-  Header: styled.div`
+  Wrapper: styled.div`
     display: flex;
-    height: ${(props) => (props.about ? "50px" : "70px")};
-    width: ${(props) => (props.about ? "95vw" : "100%")};
-    border-bottom: 1px solid lightgrey;
-    align-items: center;
-    padding-left: 15px;
-  `,
-  CommentsWrapper: styled.div`
-    width: ${(props) => (props.about ? "95vw" : "50%")};
-    height: ${(props) => (props.about ? props.about : "423px")};
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    overflow-y: scroll;
-    overflow-x: hidden;
-    ::-webkit-scrollbar {
-      display: none;
+    width: 100%;
+    height: 100%;
+    position: relative;
+    @media (max-width: 900px) {
+      flex-direction: column;
     }
   `,
-  CommentInput: styled.input`
-    appearance: none;
-    width: ${(props) => (props.about ? props.about : "429px")};
-    height: 53px;
-    border: none;
-    border-radius: none;
-    padding-left: 15px;
-    font-size: 16px;
-    background-color: inherit;
-    :focus {
-      outline: none;
-    }
-    ::placeholder {
-      color: lightgrey;
-    }
-    :-webkit-appearance {
-      display: none;
-    }
-    :-moz-appearance {
-      display: none;
-    }
-  `,
-  SubmitButton: styled.button`
-    appearance: none;
-    width: ${(props) => props.about};
-    border: none;
-    background-color: inherit;
-    font-weight: bold;
-    color: ${(props) => props.color};
-    cursor: pointer;
-    :-webkit-appearance {
-      display: none;
-    }
-    :-moz-appearance {
-      display: none;
+  ImgWrapper: styled.div`
+    width: 50%;
+    height: 100%;
+    @media (max-width: 900px) {
+      width: 100%;
+      height: 30vh;
     }
   `,
   Img: styled.img`
     width: 100%;
     height: 100%;
   `,
-  CommentInputArea: styled.form`
-    position: absolute;
-    bottom: 0;
-    right: 0;
-    border-top: 1px solid lightgrey;
+  DetailContainer: styled.div`
     display: flex;
-    width: ${(props) => props.about};
+    flex-direction: column;
+    width: 50%;
+    height: auto;
+    @media (max-width: 900px) {
+      width: 95vw;
+    }
+  `,
+  IconContainer: styled.div`
+    display: flex;
+    width: 100%;
+    height: fit-content;
+    justify-content: flex-start;
+    align-items: center;
   `,
 }
 
@@ -116,30 +84,13 @@ export default function CommentModal({ isOpen, setIsOpen, feedData }: Props) {
       title={"이미지 상세"}
       isPC={true}
     >
-      <FlexBox
-        width={"100%"}
-        height={"100%"}
-        style={{ position: "relative" }}
-        column={windowSize < 900 ? true : false}
-      >
-        <FlexBox
-          width={windowSize < 900 ? "100%" : "50%"}
-          height={windowSize < 900 ? "30vh" : "100%"}
-        >
+      <Style.Wrapper>
+        <Style.ImgWrapper>
           <Style.Img src={feedData.imageUrl} alt="image" />
-        </FlexBox>
-        <FlexBox
-          column={true}
-          width={windowSize < 900 ? "95vw" : "50%"}
-          height={"auto"}
-        >
+        </Style.ImgWrapper>
+        <Style.DetailContainer>
           <CommentList feedData={feedData} commentAreaRef={commentAreaRef} />
-          <FlexBox
-            width={"100%"}
-            height={"fit-content"}
-            justifyContents="flex-start"
-            alignItems="center"
-          >
+          <Style.IconContainer>
             <Margin direction="row" size={10} />
             {likerList !== undefined &&
             authService.currentUser !== null &&
@@ -157,7 +108,7 @@ export default function CommentModal({ isOpen, setIsOpen, feedData }: Props) {
             />
             <Margin direction="row" size={15} />
             <ShareIcon />
-          </FlexBox>
+          </Style.IconContainer>
           <Margin direction="column" size={10} />
           <FlexBox style={{ paddingLeft: "10px" }} gap={15}>
             {likerList !== undefined ? (
@@ -171,8 +122,8 @@ export default function CommentModal({ isOpen, setIsOpen, feedData }: Props) {
               <CustomH6>댓글 0개</CustomH6>
             )}
           </FlexBox>
-        </FlexBox>
-      </FlexBox>
+        </Style.DetailContainer>
+      </Style.Wrapper>
       <CommentInput
         feedData={feedData}
         inputRef={inputRef}
