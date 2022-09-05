936e284143eb60dc3a8f8a729b8156a5af956571
kyw0716 <kyw0716@naver.com>
2022-09-05 18:30:56 +0900

    fix: 모바일 dm input창 보이지 않는 버그 수정
    한번 더 수정

--- firebase-practice/src/components/layout/index.tsx
+++ firebase-practice/src/components/layout/index.tsx
@@ -19,7 +19,7 @@ const Style = {
 export default function Layout({ children, isMobileDM }: Props) {
   return (
     <Style.Wrapper
-      style={{ minHeight: isMobileDM ? "100vh" : "calc(100vh + 60px)" }}
+      style={{ minHeight: isMobileDM ? "100vh - 60px" : "calc(100vh + 60px)" }}
     >
       <Header />
       {children}
