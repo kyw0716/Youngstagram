40655bd08094cf194ac54e9fc46b571f4730918a
kyw0716 <kyw0716@naver.com>
2022-09-17 14:30:47 +0900

    feat: 마이페이지 피드 카드 렌더링 최적화
    컴포넌트 분리

---
+++ firebase-practice/src/components/share/Feed/FeedSortingCard/LikeCommentInfo.tsx
@@ -0,0 +1,69 @@
+import { DBService } from "@FireBase"
+import FollowListModal from "@share/Modal/follow/FollowListModal"
+import { userDataState } from "@share/recoil/recoilList"
+import { FeedData } from "backend/dto"
+import { doc, onSnapshot } from "firebase/firestore"
+import React, { SetStateAction, useEffect, useState } from "react"
+import { useRecoilValue } from "recoil"
+import { CustomH6, FlexBox } from "ui"
+
+type Props = {
+  feedData: FeedData
+  setIsCurrentUserLike: React.Dispatch<SetStateAction<boolean>>
+}
+
+export default function LikeCommentInfo({
+  feedData,
+  setIsCurrentUserLike,
+}: Props) {
+  const [commentData, setCommentData] = useState<Comment[]>([])
+  const [likerList, setLikerList] = useState<string[]>([])
+  const [isLikerListModalOpen, setIsLikerListModalOpen] =
+    useState<boolean>(false)
+  const currentUser = useRecoilValue(userDataState)
+
+  useEffect(() => {
+    onSnapshot(doc(DBService, "Comments", `${feedData.storageId}`), (doc) => {
+      setCommentData(doc.data()?.AllComments)
+    })
+    onSnapshot(doc(DBService, "like", `${feedData.storageId}`), (doc) => {
+      setLikerList(doc.data()?.likerList)
+    })
+  }, [feedData])
+
+  useEffect(() => {
+    if (!likerList) return
+    if (!currentUser) return
+    if (currentUser.info.userId === "") return
+    if (likerList.includes(currentUser.info.userId)) setIsCurrentUserLike(true)
+    else setIsCurrentUserLike(false)
+  }, [likerList, currentUser])
+
+  return (
+    <FlexBox style={{ paddingLeft: "10px", marginTop: "-10px" }} gap={10}>
+      <FollowListModal
+        userList={likerList}
+        isOpen={isLikerListModalOpen}
+        setIsOpen={setIsLikerListModalOpen}
+        title={"좋아요"}
+      />
+      {likerList !== undefined ? (
+        <CustomH6
+          style={{ cursor: "pointer" }}
+          onClick={() => {
+            setIsLikerListModalOpen(true)
+          }}
+        >
+          좋아요 {likerList.length}개
+        </CustomH6>
+      ) : (
+        <CustomH6>좋아요 0개</CustomH6>
+      )}
+      {commentData !== undefined ? (
+        <CustomH6>댓글 {commentData.length}개</CustomH6>
+      ) : (
+        <CustomH6>댓글 0개</CustomH6>
+      )}
+    </FlexBox>
+  )
+}
---
+++ firebase-practice/src/components/share/Feed/FeedSortingCard/index.tsx
@@ -0,0 +1,155 @@
+import { DBService } from "@FireBase"
+import { userDataState } from "@share/recoil/recoilList"
+import { FeedData, UserInfo } from "backend/dto"
+import { doc, onSnapshot } from "firebase/firestore"
+import { ProfileIcon } from "icons"
+import Image from "next/image"
+import { SetStateAction, useEffect, useState } from "react"
+import { useRecoilValue } from "recoil"
+import styled from "styled-components"
+import { FlexBox, Margin } from "ui"
+import Icons from "../FeedCard/Icons"
+import Desc from "./Desc"
+import LikeCommentInfo from "./LikeCommentInfo"
+import ThreeDotMenu from "./ThreeDotMenu"
+
+type Props = {
+  feedData: FeedData
+  setIsCommentModalOpen: React.Dispatch<SetStateAction<boolean>>
+  setIsFeedUploadModalOpen: React.Dispatch<SetStateAction<boolean>>
+}
+
+const Style = {
+  ImageHeader: styled.div`
+    width: 470px;
+    height: 58px;
+    display: flex;
+    align-items: center;
+    padding-left: 15px;
+    justify-content: space-between;
+    border-radius: 10px;
+    border-bottom: none;
+    position: relative;
+    @media (max-width: 500px) {
+      width: 95%;
+      padding: 0px 5px;
+    }
+  `,
+  HeaderText: styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 38px;
+    justify-content: center;
+  `,
+  UserName: styled.span`
+    font-size: 12px;
+    font-weight: bold;
+    color: black;
+  `,
+  ImageTitle: styled.span`
+    font-size: 7px;
+    font-weight: 400;
+    color: gray;
+  `,
+  ImageCard: styled.div`
+    width: 470px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid lightgrey;
+    border-radius: 10px;
+    padding-bottom: 10px;
+    background-color: white;
+    max-width: 470px;
+    @media (max-width: 900px) {
+      width: 95%;
+    }
+  `,
+}
+
+export default function FeedSortingCard({
+  feedData,
+  setIsCommentModalOpen,
+  setIsFeedUploadModalOpen,
+}: Props) {
+  const userData = useRecoilValue(userDataState)
+  const [creatorInfo, setCreatorInfo] = useState<UserInfo>()
+  const [isCurrentUserLike, setIsCurrentUserLike] = useState<boolean>(false)
+
+  useEffect(() => {
+    onSnapshot(doc(DBService, "users", `${feedData.creator}`), (doc) => {
+      setCreatorInfo(doc.data()?.info)
+    })
+  }, [feedData])
+
+  return (
+    <>
+      {userData && (
+        <Style.ImageCard>
+          <Style.ImageHeader>
+            <FlexBox
+              width={"fit-content"}
+              height={58}
+              gap={15}
+              alignItems={"center"}
+            >
+              {userData.info.profileImage ? (
+                <Image
+                  src={userData?.info.profileImage}
+                  alt="creator"
+                  width={38}
+                  height={38}
+                  style={{ borderRadius: 38, cursor: "pointer" }}
+                />
+              ) : (
+                <ProfileIcon width={38} height={38} />
+              )}
+              <Style.HeaderText>
+                <Style.UserName>{userData?.info.name}</Style.UserName>
+                <Style.ImageTitle>{feedData.location}</Style.ImageTitle>
+              </Style.HeaderText>
+            </FlexBox>
+            <ThreeDotMenu
+              feedData={feedData}
+              setIsFeedUploadModalOpen={setIsFeedUploadModalOpen}
+            />
+          </Style.ImageHeader>
+          {feedData.imageUrl ? (
+            <Image
+              src={feedData.imageUrl}
+              width={470}
+              height={600}
+              alt="Image"
+              priority
+            />
+          ) : (
+            <Image
+              src="https://giphy.com/embed/wnYB3vx9t6PXiq1ubB"
+              width={470}
+              height={600}
+              alt="Image"
+            />
+          )}
+
+          <Margin direction="column" size={10} />
+          <Icons
+            isCurrentUserLike={isCurrentUserLike}
+            setIsCommentModalOpen={setIsCommentModalOpen}
+            feedData={feedData}
+          />
+          <Margin direction="column" size={15} />
+          <LikeCommentInfo
+            feedData={feedData}
+            setIsCurrentUserLike={setIsCurrentUserLike}
+          />
+          <Desc
+            feedData={feedData}
+            setIsCommentModalOpen={setIsCommentModalOpen}
+            name={creatorInfo?.name}
+          />
+        </Style.ImageCard>
+      )}
+    </>
+  )
+}
