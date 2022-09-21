6e722e9476f97836b8abe4bbb8d9531e6cbbf243
kyw0716 <kyw0716@naver.com>
2022-09-21 13:32:23 +0900

    feat: drakmode 적용
    마이페이지

--- firebase-practice/src/icons.tsx
+++ firebase-practice/src/icons.tsx
@@ -224,24 +224,27 @@ export function CameraIcon({ width, height }: Props) {
 }
 
 export function DeleteIcon({ width, height, onClick }: Props) {
+  const isDarkMode = useRecoilValue(darkModeState)
   return (
     <svg
       width={width}
       height={height}
       viewBox="0 0 24 24"
       fill="none"
+      stroke={isDarkMode ? "white" : ""}
       xmlns="http://www.w3.org/2000/svg"
       style={{ cursor: "pointer" }}
     >
       <path
         d="M7 4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v2h4a1 1 0 1 1 0 2h-1.069l-.867 12.142A2 2 0 0 1 17.069 22H6.93a2 2 0 0 1-1.995-1.858L4.07 8H3a1 1 0 0 1 0-2h4V4zm2 2h6V4H9v2zM6.074 8l.857 12H17.07l.857-12H6.074zM10 10a1 1 0 0 1 1 1v6a1 1 0 1 1-2 0v-6a1 1 0 0 1 1-1zm4 0a1 1 0 0 1 1 1v6a1 1 0 1 1-2 0v-6a1 1 0 0 1 1-1z"
-        fill="#0D0D0D"
+        fill={isDarkMode ? "white" : "#0D0D0D"}
       />
     </svg>
   )
 }
 
-export function EditIcon({ width, height, onClick }: Props) {
+export function EditIcon({ width, height }: Props) {
+  const isDarkMode = useRecoilValue(darkModeState)
   return (
     <svg
       version="1.1"
@@ -255,6 +258,8 @@ export function EditIcon({ width, height, onClick }: Props) {
       width={width}
       height={height}
       style={{ cursor: "pointer" }}
+      fill={isDarkMode ? "white" : ""}
+      stroke={isDarkMode ? "white" : ""}
     >
       <g>
         <g>
