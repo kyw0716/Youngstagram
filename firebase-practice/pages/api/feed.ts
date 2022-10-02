030100c96fd2095f526f575a3b74863e10d3870b
kyw0716 <kyw0716@naver.com>
2022-10-02 10:31:30 +0900

    feat: next.js api 연습

---
+++ firebase-practice/pages/api/feed.ts
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { DBService } from "@FireBase"
+import { FeedData } from "backend/dto"
+import { doc, getDoc } from "firebase/firestore"
+import type { NextApiRequest, NextApiResponse } from "next"
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<FeedData[]>,
+) {
+  if (req.method === "GET") {
+    const getFeedRef = doc(DBService, "mainPage", "userFeedDataAll")
+    const docSnapShot = await getDoc(getFeedRef)
+
+    if (docSnapShot.exists()) {
+      const data = docSnapShot.data()?.feed as FeedData[]
+      res.status(200).json(data)
+    }
+  }
+}
