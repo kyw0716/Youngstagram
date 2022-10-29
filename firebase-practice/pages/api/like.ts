2f5b9adf37f5ede0ef467fb11a44c3d030a5534d
kyw0716 <kyw0716@naver.com>
2022-10-29 14:01:51 +0900

    feat: 좋아요 정보 요청 api 추가

---
+++ firebase-practice/pages/api/like.ts
@@ -0,0 +1,26 @@
+import { DBService } from "@FireBase"
+import { UserData } from "backend/dto"
+import { doc, getDoc } from "firebase/firestore"
+import { NextApiRequest, NextApiResponse } from "next"
+
+/**
+ * request url : /api/like?storageId=${이미지 저장 스토리지 아이디}
+ * method : GET
+ */
+
+export default async function getLike(
+  req: NextApiRequest,
+  res: NextApiResponse<string[]>,
+) {
+  if (req.method === "GET") {
+    const storageId = req.query?.storageId
+    console.log(storageId)
+    const getLikeRef = doc(DBService, "like", `${storageId}`)
+    const docSnapShot = await getDoc(getLikeRef)
+
+    if (docSnapShot.exists()) {
+      const data = docSnapShot.data()?.likerList
+      res.status(200).json(data as string[])
+    }
+  }
+}
