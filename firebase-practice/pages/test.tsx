be77927d58e4be02f3e70216464b0991e434752a
kyw0716 <kyw0716@naver.com>
2022-10-30 16:14:12 +0900

    test: 이미지 업로드 테스트 코드 추가

--- firebase-practice/pages/test.tsx
+++ firebase-practice/pages/test.tsx
@@ -1,15 +1,39 @@
 import Loading from "@share/Loading/Loading"
+import { userDataState } from "@share/recoil/recoilList"
+import axios from "axios"
 import Layout from "components/layout"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { useRecoilValue } from "recoil"
 
 export default function Test() {
+  const userData = useRecoilValue(userDataState)
+  const [image, setImage] = useState<File>()
+
   useEffect(() => {
-    fetch("/api/like?storageId=37958bfa-a656-4536-b596-62e0bfbe662f")
-      .then((response) => response.json())
-      .then((json) => console.log(json))
-  }, [])
+    if (image) {
+      axios({
+        method: "POST",
+        url: "/api/imageUpload",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          imageFile: image,
+          creator: userData.info.userId,
+        },
+      }).then((response) => console.log(response.data))
+    }
+  }, [image])
   return (
     <Layout>
+      <input
+        type={"file"}
+        onChange={(e) => {
+          if (e.target.files !== null) {
+            setImage(e.target.files[0])
+          }
+        }}
+      />
       <Loading width={150} height={150} borderRadius={150} />
     </Layout>
   )
