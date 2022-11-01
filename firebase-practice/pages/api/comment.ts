8cbd3656b62603e272a07506285b739f9daeac08
kyw0716 <kyw0716@naver.com>
2022-11-02 01:13:44 +0900

    feat: comment api 완성
    등록, 수정, 삭제 기능 모두 구현

--- firebase-practice/pages/api/comment.ts
+++ firebase-practice/pages/api/comment.ts
@@ -1,12 +1,35 @@
 import { DBService } from "@FireBase"
 import { Comment, UserData } from "backend/dto"
-import { doc, getDoc } from "firebase/firestore"
+import {
+  arrayRemove,
+  arrayUnion,
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore"
+import getCurrentTime from "lib/getCurrentTime"
 import { NextApiRequest, NextApiResponse } from "next"
+import { v4 } from "uuid"
 
 /**
- * request url : /api/comment?commentId=${코멘트 아이디}
  * method : GET
+ * request url : /api/comment?commentId=${코멘트 아이디}
  * response : commentId를 가진 피드의 코멘트 정보 배열
+ *
+ * method : POST
+ * request url : /api/comment
+ * request data : {commentId?, oldComment?, uploadTime?, userId, comment, feedId}
+ * response : Success
+ *
+ * update : commentId, oldComment, uploadTime 모두 포함한 데이터 전송
+ * new comment : userId, comment, feedId 만을 포함한 데이터 전송
+ *
+ *
+ * method : DELETE
+ * request url : /api/comment
+ * request data : {commentId, comment, uploadTime, userId, feedId}
+ * response : Success
  */
 
 export default async function getComment(
@@ -25,5 +48,78 @@ export default async function getComment(
     } else {
       res.status(500).json("Fail")
     }
+  }
+  if (req.method === "POST") {
+    const { commentId, userId, comment, feedId, uploadTime, oldComment } =
+      req.body
+    const commentRef = doc(DBService, "Comments", feedId)
+
+    if (commentId) {
+      const commentFormToRemove: Comment = {
+        userId: userId,
+        commentId: commentId,
+        comment: oldComment,
+        uploadTime: uploadTime,
+      }
+      const commentFormToUpdate: Comment = {
+        userId: userId,
+        commentId: commentId,
+        comment: comment,
+        uploadTime: uploadTime,
+      }
+      await updateDoc(commentRef, {
+        AllComments: arrayRemove(commentFormToRemove),
+      })
+        .catch((error) => res.status(500).json(error.code))
+        .then(async () => {
+          await updateDoc(commentRef, {
+            AllComments: arrayUnion(commentFormToUpdate),
+          })
+            .catch((error) => res.status(500).json(error.code))
+            .then(() => {
+              res.status(200).json("Success")
+            })
+        })
+    } else {
+      const commentForm: Comment = {
+        userId: userId,
+        commentId: v4(),
+        comment: comment,
+        uploadTime: getCurrentTime(),
+      }
+      await updateDoc(commentRef, {
+        AllComments: arrayUnion(commentForm),
+      })
+        .catch(async (error) => {
+          if (error.code === "not-found") {
+            await setDoc(commentRef, {
+              AllComments: [commentForm],
+            }).catch((error) => res.status(500).json(error.code))
+          } else {
+            res.status(500).json(error.code)
+          }
+        })
+        .then(() => {
+          res.status(200).json("Success")
+        })
+    }
+  }
+  if (req.method === "DELETE") {
+    const { commentId, userId, comment, feedId, uploadTime } = req.body
+    const commentRef = doc(DBService, "Comments", feedId)
+    const commentFormToRemove: Comment = {
+      userId: userId,
+      commentId: commentId,
+      comment: comment,
+      uploadTime: uploadTime,
+    }
+
+    await updateDoc(commentRef, {
+      AllComments: arrayRemove(commentFormToRemove),
+    })
+      .catch((error) => res.status(500).json(error.code))
+      .then(() => {
+        res.status(200).json("Success")
+      })
   }
 }
