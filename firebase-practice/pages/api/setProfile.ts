8a73e126928c98d4129a368e5d4caa7dbb360853
kyw0716 <kyw0716@naver.com>
2022-11-01 23:13:30 +0900

    feat: 프로필 등록 api 추가 (테스트 완료)

--- firebase-practice/pages/api/setProfile.ts
+++ firebase-practice/pages/api/setProfile.ts
@@ -1,4 +1,4 @@
-import { DBService } from "@FireBase"
+import { authService, DBService } from "@FireBase"
 import { UserData, UserInfo } from "backend/dto"
 import { updateProfile } from "firebase/auth"
 import { doc, updateDoc } from "firebase/firestore"
@@ -17,12 +17,12 @@ import { NextApiRequest, NextApiResponse } from "next"
  * @response userId를 가진 유저의 프로필 정보
  */
 
-export default async function getProfile(
+export default async function setProfile(
   req: NextApiRequest,
   res: NextApiResponse<UserData | string>,
 ) {
   if (req.method === "POST") {
-    const { userId, userName, profileImage, email, currentUser } = req.body
+    const { userId, userName, profileImage, email } = req.body
     const setProfileRef = doc(DBService, "users", `${userId}`)
 
     const profileForm: UserInfo = {
@@ -32,14 +32,17 @@ export default async function getProfile(
       email: email,
     }
 
-    await updateProfile(currentUser, {
-      displayName: userName,
-      photoURL: profileImage,
-    }).catch((error) => res.status(500).json(error.code))
+    if (authService.currentUser)
+      await updateProfile(authService.currentUser, {
+        displayName: userName,
+        photoURL: profileImage,
+      }).catch((error) => res.status(500).json(error.code))
     await updateDoc(setProfileRef, {
       info: profileForm,
-    }).catch((error) => res.status(500).json(error.code))
-
-    res.status(200).json("Success")
+    })
+      .catch((error) => res.status(500).json(error.code))
+      .then(() => {
+        res.status(200).json("Success")
+      })
   }
 }
