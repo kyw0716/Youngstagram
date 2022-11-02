396c3b7289a887471e5b5cb07673c422b2cb98d1
kyw0716 <kyw0716@naver.com>
2022-11-02 15:30:21 +0900

    feat: 피드 api에 피드 수정 기능 추가

--- firebase-practice/pages/api/feed.ts
+++ firebase-practice/pages/api/feed.ts
@@ -8,6 +8,7 @@ import {
   setDoc,
   updateDoc,
 } from "firebase/firestore"
+import getCurrentTime from "lib/getCurrentTime"
 import type { NextApiRequest, NextApiResponse } from "next"
 
 /**
@@ -18,8 +19,13 @@ import type { NextApiRequest, NextApiResponse } from "next"
  * TODO: 새로운 피드 등록과 피드 수정 기능 구분하기
  * method : POST
  * request url : /api/feed
- * request body : {imageUrl, desc, location, isPrivate, storageId, creator, uploadTime}
- * response: "Success"
+ * request body for new feed : {imageUrl, desc, location, isPrivate, creator}
+ * response : Success
+ *
+ * method : POST
+ * request url : /api/feed
+ * request body for new feed : {imageUrl, desc, location, isPrivate, storageId, creator, uploadTime, newDesc, newLocation, newIsPrivate}
+ * response : Success
  *
  * method : DELETE
  * request url : /api/feed
@@ -52,46 +58,90 @@ export default async function getFeed(
       storageId,
       creator,
       uploadTime,
-    } = req.body as FeedData
+      newDesc,
+      newLocation,
+      newIsPrivate,
+    } = req.body
 
     const firestoreAllRef = doc(DBService, "mainPage", `userFeedDataAll`)
     const firestorePersonalRef = doc(DBService, "users", `${creator}`)
 
-    const feed: FeedData = {
-      imageUrl: imageUrl,
-      desc: desc,
-      location: location,
-      isPrivate: isPrivate,
-      storageId: storageId,
-      creator: creator,
-      uploadTime: uploadTime,
+    if (!uploadTime) {
+      const feed: FeedData = {
+        imageUrl: imageUrl,
+        desc: desc,
+        location: location,
+        isPrivate: isPrivate,
+        storageId: storageId,
+        creator: creator,
+        uploadTime: getCurrentTime(),
+      }
+
+      await updateDoc(firestoreAllRef, {
+        feed: arrayUnion(feed),
+      }).catch(async (error) => {
+        if (error.code === "not-found") {
+          await setDoc(firestoreAllRef, {
+            feed: [feed],
+          })
+        } else {
+          res.status(500).json(error.code)
+        }
+      })
+
+      await updateDoc(firestorePersonalRef, {
+        feed: arrayUnion(feed),
+      }).catch(async (error) => {
+        if (error.code === "not-found") {
+          await setDoc(firestorePersonalRef, {
+            feed: [feed],
+          })
+        } else {
+          res.status(500).json(error.code)
+        }
+      })
+
+      res.status(200).json("Success")
+    } else {
+      const feedToRemove: FeedData = {
+        imageUrl: imageUrl,
+        desc: desc,
+        location: location,
+        isPrivate: isPrivate,
+        storageId: storageId,
+        creator: creator,
+        uploadTime: uploadTime,
+      }
+      const newFeed: FeedData = {
+        imageUrl: imageUrl,
+        desc: newDesc,
+        location: newLocation,
+        isPrivate: newIsPrivate,
+        storageId: storageId,
+        creator: creator,
+        uploadTime: uploadTime,
+      }
+
+      await updateDoc(firestoreAllRef, {
+        feed: arrayRemove(feedToRemove),
+      }).catch((error) => res.status(500).json(error.code))
+
+      await updateDoc(firestorePersonalRef, {
+        feed: arrayRemove(feedToRemove),
+      }).catch((error) => {
+        res.status(500).json(error.code)
+      })
+
+      await updateDoc(firestoreAllRef, {
+        feed: arrayUnion(newFeed),
+      }).catch((error) => res.status(500).json(error.code))
+
+      await updateDoc(firestorePersonalRef, {
+        feed: arrayUnion(newFeed),
+      }).catch((error) => res.status(500).json(error.code))
+
+      res.status(200).json("Success")
     }
-
-    await updateDoc(firestoreAllRef, {
-      feed: arrayUnion(feed),
-    }).catch(async (error) => {
-      if (error.code === "not-found") {
-        await setDoc(firestoreAllRef, {
-          feed: [feed],
-        })
-      } else {
-        res.status(500).json(error.code)
-      }
-    })
-
-    await updateDoc(firestorePersonalRef, {
-      feed: arrayUnion(feed),
-    }).catch(async (error) => {
-      if (error.code === "not-found") {
-        await setDoc(firestorePersonalRef, {
-          feed: [feed],
-        })
-      } else {
-        res.status(500).json(error.code)
-      }
-    })
-
-    res.status(200).json("Success")
   } else if (req.method === "DELETE") {
     const {
       imageUrl,
